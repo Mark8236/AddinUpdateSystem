cd2c148c8164b953d0aced252f3062c3f1549891
Mark8236 <mark8236@hotmail.com>
2016-06-27 17:32:25 +0900

    Add project files.

---
+++ AddinUpdateSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AddinUpdateSystem.MvcApplication" Language="C#" %>
---
+++ AddinUpdateSystem/app/main.ts
@@ -0,0 +1,6 @@
+﻿import {bootstrap}    from '@angular/platform-browser-dynamic'
+
+import { AppComponent } from './app.component';
+
+//enableProdMode();
+bootstrap(AppComponent); 
